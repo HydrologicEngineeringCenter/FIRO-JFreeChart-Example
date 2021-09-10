01245c82f4837174ee42d378ddc8bb435d7a4233
Neema Nejad <Neema.G.Nejad@usace.army.mil>
2021-09-10 07:32:31 -0700

    main class now shows plot with a line a dots

--- src/MultipleLineChartExample.java
+++ src/MultipleLineChartExample.java
@@ -1,62 +1,120 @@
-import javax.swing.JFrame;
-import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.general.Series;
+import org.jfree.data.xy.DefaultXYDataset;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
-import java.util.Random;
+import javax.swing.*;
+import java.awt.*;
 
+/**
+ * @author imssbora
+ *
+ */
 public class MultipleLineChartExample extends JFrame {
 
-    public MultipleLineChartExample(String title) {
-        super(title);
-        // Create dataset
-        DefaultCategoryDataset dataset = createDataset();
-        // Create chart
-        JFreeChart chart = ChartFactory.createLineChart(
-                "Forecasts", // Chart title
-                "Date", // X-Axis Label
-                "Values", // Y-Axis Label
-                dataset
-        );
-
-        ChartPanel panel = new ChartPanel(chart);
-        setContentPane(panel);
+    public MultipleLineChartExample(){
+        initUI();
     }
 
-    private DefaultCategoryDataset createDataset() {
+    private void initUI() {
 
-        String series1 = "Forecast 1";
-        String series2 = "Forecast 2";
+        JFreeChart chart = createChart();
 
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        chartPanel.setBackground(Color.white);
+        add(chartPanel);
 
-        dataset.addValue(200, series1, "2016-12-19");
-        dataset.addValue(150, series1, "2016-12-20");
-        dataset.addValue(100, series1, "2016-12-21");
-        dataset.addValue(210, series1, "2016-12-22");
-        dataset.addValue(240, series1, "2016-12-23");
-        dataset.addValue(195, series1, "2016-12-24");
-        dataset.addValue(245, series1, "2016-12-25");
+        pack();
+        setTitle("Line chart");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
-        dataset.addValue(150, series2, "2016-12-19");
-        dataset.addValue(130, series2, "2016-12-20");
-        dataset.addValue(95, series2, "2016-12-21");
-        dataset.addValue(195, series2, "2016-12-22");
-        dataset.addValue(200, series2, "2016-12-23");
-        dataset.addValue(180, series2, "2016-12-24");
-        dataset.addValue(230, series2, "2016-12-25");
+    public JFreeChart createChart(){
+        // Create a single plot containing both the scatter and line
+        XYPlot plot = new XYPlot();
+
+        /* SETUP SCATTER */
+
+        // Create the scatter data, renderer, and axis
+        XYDataset collection1 = getScatterPlotData();
+        XYItemRenderer renderer1 = new XYLineAndShapeRenderer(false, true);	// Shapes only
+        ValueAxis domain1 = new NumberAxis("Domain1");
+        ValueAxis range1 = new NumberAxis("Range1");
+
+        // Set the scatter data, renderer, and axis into plot
+        plot.setDataset(0, collection1);
+        plot.setRenderer(0, renderer1);
+        plot.setDomainAxis(0, domain1);
+        plot.setRangeAxis(0, range1);
+
+        // Map the scatter to the first Domain and first Range
+        plot.mapDatasetToDomainAxis(0, 0);
+        plot.mapDatasetToRangeAxis(0, 0);
+
+        /* SETUP LINE */
+
+        // Create the line data, renderer, and axis
+        XYDataset collection2 = getLinePlotData();
+        XYItemRenderer renderer2 = new XYLineAndShapeRenderer(true, false);	// Lines only
+        ValueAxis domain2 = new NumberAxis("Domain2");
+        ValueAxis range2 = new NumberAxis("Range2");
+
+        // Set the line data, renderer, and axis into plot
+        plot.setDataset(1, collection2);
+        plot.setRenderer(1, renderer2);
+        plot.setDomainAxis(1, domain2);
+        plot.setRangeAxis(1, range2);
+
+        // Map the line to the second Domain and second Range
+        plot.mapDatasetToDomainAxis(1, 1);
+        plot.mapDatasetToRangeAxis(1, 1);
+
+        // Create the chart with the plot and a legend
+        return new JFreeChart("Multi Dataset Chart", JFreeChart.DEFAULT_TITLE_FONT, plot, true);
+    }
+
+    public static void main(String[] args) {
+        MultipleLineChartExample ex = new MultipleLineChartExample();
+        ex.setVisible(true);
+    }
+
+    private static XYDataset getScatterPlotData() {
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        XYSeries series1 = new XYSeries("Version 1");
+
+        series1.add(1.0, 2.0);
+        series1.add(2.0, 3.0);
+        series1.add(3.0, 2.5);
+        series1.add(3.5, 2.8);
+        series1.add(4.2, 6.0);
+
+        dataset.addSeries(series1);
 
         return dataset;
     }
 
-    public static void main(String[] args) {
-        MultipleLineChartExample example = new MultipleLineChartExample("Chart Example");
-        example.setAlwaysOnTop(true);
-        example.pack();
-        example.setSize(600, 400);
-        example.setVisible(true);
+    private static XYDataset getLinePlotData() {
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        XYSeries series1 = new XYSeries("Version 2");
+
+        series1.add(1.2, 4.0);
+        series1.add(2.5, 4.4);
+        series1.add(3.8, 4.2);
+        series1.add(4.3, 3.8);
+        series1.add(4.5, 4.0);
+
+        dataset.addSeries(series1);
+
+        return dataset;
     }
-}  
+}
